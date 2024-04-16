6fc8f87486fcf6916ae9f394d684e0b371e5bfcd
m-hayashi13 <m-hayashi@advanced-media.co.jp>
2024-04-16 15:48:13 +0900

    cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,28 @@
-//import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from './mdsvex.config.js';
+import { preprocess } from 'svelte/compiler';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		}),
 		// remove this if you're not using comment system
 		csp: { mode: 'auto' },
 	},
-	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)]
 };
 
 export default config;
